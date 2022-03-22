920d244c210ee82ecdec444181372585f32db6fb
Aylaris <aylarismorel@gmail.com>
2022-03-22 14:05:35 +0100

    le format s'applique à la pade d'accueil et les nom de catégories sont des liens

--- views/home.php
+++ views/home.php
@@ -5,18 +5,25 @@
         </h1>
     </div>
     <?php
-    foreach ($arrayXML as $key => $value) { ?>
-
-    <h2 class="titleCategory"><?=$value->nameCategory?></h2>
+    foreach ($arrayXML as $key => $value) { 
+    // value = catégorie
+    ?>
+    <a href="http://myrssfeed.localhost/controllers/pages-controller.php?category=<?=$key?>" class="catLink">
+        <h2 class="titleCategory"><?=$value->nameCategory?></h2>
+    </a>
     <div class="cartegoryArticle">
     <?php
-    foreach (($value->flux->channel->item) as $key => $value) {  ?>
+    $items = $value->flux->channel->item;
+    for ($i=0; $i<$format ; $i++) { 
+    // foreach (($value->flux->channel->item) as $key => $value) {  
+    // value = item
+    ?>
             <div class="cardArticle">
-                <a href="<?=$value->link?>" target="_blank" rel="noopener noreferrer">
-                    <img src="<?=$value->enclosure['url']?>" class="pictureCardArticle" alt="...">
+                <a href="<?=$items[$i]->link?>" target="_blank" rel="noopener noreferrer">
+                    <img src="<?=$items[$i]->enclosure['url']?>" class="pictureCardArticle" alt="...">
                 </a>
                 <h5 class="descriptionCardArticle">
-                    <?=$value->title?>
+                    <?=$items[$i]->title?>
                 </h5>
             </div>
     
